54a423b9d2cc8bce6a128a31e148abb463195ee6
black-techie <willbroadnyirenda@gmail.com>
2022-09-16 21:02:51 +0300

    another

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 })
 
 app.get("/", (req, res) =>{
-    return "hallowen";
+    return res.status(200).json({test: "control"})
 })
 app.post("/api/register", (req, res) => {
     let promise = insert(req.body)
